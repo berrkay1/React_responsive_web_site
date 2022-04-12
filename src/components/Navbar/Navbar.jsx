425a08770e43f6df7d5ed010d7021a9024c5b98c
BERKAY GÜL <berrkaygul@gmail.com>
2022-04-12 20:16:49 +0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {FaTimes} from 'react-icons';
+import {CgMenuRight} from 'react-icons';
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
